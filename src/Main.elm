8d7c36edd8e7fc9c09dffb8a7e4a0f95cc1711da
11bus11 <02linfor01@gmail.com>
2023-04-11 20:25:18 +0200

    starting the app

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,27 @@
 module Main exposing (main)
 
 import Browser
-import HelloWorld exposing (helloWorld)
-import Html exposing (Html, div, img)
-import Html.Attributes exposing (src, style)
-import Msg exposing (Msg(..))
+import Html.Styled exposing (Html, div, img)
+import Html.Styled.Attributes exposing (src, style)
 import VitePluginHelper
+
+type Msg = NoOp
 
 
 main : Program () Int Msg
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = 0, update = update, view = view >> Html.Styled.toUnstyled }
+
 
 
 update : Msg -> number -> number
 update msg model =
     case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
+        NoOp -> model
 
 
 view : Int -> Html Msg
 view model =
     div []
         [ img [ src <| VitePluginHelper.asset "/src/assets/logo.png", style "width" "300px" ] []
-        , helloWorld model
         ]
